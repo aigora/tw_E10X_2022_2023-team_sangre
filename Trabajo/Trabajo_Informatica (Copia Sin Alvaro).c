59e6611ae53ed5ea77142f4c7a606c06c3fd3867
Álvaro Cuesta Sanz <alvaro.cuesta@alumnos.upm.es>
2023-05-29 18:23:26 +0200

    Parte Álvaro (no ejecuta)

--- Trabajo/Trabajo_Informatica (Copia Sin Alvaro).c
+++ Trabajo/Trabajo_Informatica (Copia Sin Alvaro).c
@@ -137,6 +137,10 @@ void f_comparacion_ficheros (int anio_mes_void, int mes_void, int *hidraulica_0,
 /*Funcion para predicciones*/
 void predicciones();
 
+/*Funciones Álvaro*/
+
+ void Op_Ficheros_Anios( int anio_1, int anio_2);
+ 
 
 //COMIENZO CON EL CREAR CUENTA O INICIO DE SESION
 int main()
@@ -2914,6 +2918,27 @@ void f_comparacion () /*Funcion Comparacion*/
     {
         case 'a': // Parte de Alvaro//
         {
+        	int anio1, anio2;
+              anio1 =0;
+              anio2 =0;
+        	
+             do {
+             	printf("Nuestra base de datos contiene datos por anio desde el periodo del 2007 hasta el 2023. Recuerde elegir un anio en ese intervalo.\n\n");
+                printf("Ingrese el primer anio entre 2007 y 2023: ");
+                scanf(" %d",&anio1);
+                } while (anio1 < 2007 && anio1 > 2023);
+
+             do {
+                printf("Ingrese el segundo anio entre 2007 y 2023: ");
+                scanf(" %d",&anio2);
+                } while (anio2 < 2007 && anio2 > 2023);
+                
+                printf("Ha elegido comparar los datos de los anios %d y %d.\nA continuacion observara el dato individual anual de cada uno y posteriormente la diferencia", anio1, anio2);
+                
+               Op_Ficheros_Anios( anio1, anio2);
+               
+                   
+
             break;
         }
         
@@ -3089,6 +3114,147 @@ void f_comparacion () /*Funcion Comparacion*/
             break;
         }
 	}
+}
+
+void Op_Ficheros_Anios(int anio_1, int anio_2) {
+    char ruta1[100], ruta2[100];
+    sprintf(ruta1, "C:\\Users\\user2\\Desktop\\FicherosAnios\\%d.txt", anio_1);
+    sprintf(ruta2, "C:\\Users\\user2\\Desktop\\FicherosAnios\\%d.txt", anio_2);
+
+    FILE *archivo1 = fopen(ruta1, "r");
+    FILE *archivo2 = fopen(ruta2, "r");
+
+    if (archivo1 == NULL || archivo2 == NULL) {
+        printf("No se pudieron abrir uno o ambos archivos.\n");
+        return;
+    }
+
+    printf("\nInformación de la producción del año %d:\n\n", anio_1);
+    
+    int numero, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9, numero10, numero11, numero12, numero13, numero14, numero15, numero16, numero17, numero18, numero19;
+
+    fscanf(archivo1, "%d", &numero);
+    printf("Hidraulica: %d\n", numero);
+
+    fscanf(archivo1, "%d", &numero2);
+    printf("Eolica: %d\n", numero2);
+
+    fscanf(archivo1, "%d", &numero3);
+    printf("Solar Fotovoltaiva: %d\n", numero3);
+
+    fscanf(archivo1, "%d", &numero4);
+    printf("Solar Termica: %d\n", numero4);
+    
+    fscanf(archivo1, "%d", &numero5);
+    printf("Hidroelectrica: %d\n", numero5);
+    
+    fscanf(archivo1, "%d", &numero6);
+    printf("Otras Renovables: %d\n", numero6);
+
+    fscanf(archivo1, "%d", &numero7);
+    printf("Resiudos Renovables: %d\n\n", numero7);
+
+    fscanf(archivo1, "%d", &numero8);
+    printf("Generacion Renovable: %d\n\n\n", numero8);
+
+    fscanf(archivo1, "%d", &numero9);
+    printf("Turbinacion Bombeo: %d\n", numero9);
+
+    fscanf(archivo1, "%d", &numero10);
+    printf("Nuclear: %d\n", numero10);
+
+    fscanf(archivo1, "%d", &numero11);
+    printf("Ciclo Bombeado: %d\n", numero11);
+
+    fscanf(archivo1, "%d", &numero12);
+    printf("Carbon: %d\n", numero12);
+
+    fscanf(archivo1, "%d", &numero13);
+    printf("Motores Diesel: %d\n", numero13);
+
+    fscanf(archivo1, "%d", &numero14);
+    printf("Turbina de Gas: %d\n", numero14);
+
+    fscanf(archivo1, "%d", &numero15);
+    printf("Turbina de Vapor: %d\n", numero15);
+
+    fscanf(archivo1, "%d", &numero16);
+    printf("Fuel Gas: %d\n", numero16);
+
+    fscanf(archivo1, "%d", &numero17);
+    printf("Cogeneracion: %d\n", numero17);
+
+    fscanf(archivo1, "%d", &numero18);
+    printf("Residuos no renovables: %d\n\n", numero18);
+
+    fscanf(archivo1, "%d", &numero19);
+    printf("Generacion no renovable: %d\n\n\n", numero19);
+
+    printf("\n");
+
+    printf("Información de la producción del año %d:\n\n", anio_2);
+
+    fscanf(archivo2, "%d", &numero);
+    printf("Hidraulica: %d\n", numero);
+
+    fscanf(archivo2, "%d", &numero2);
+    printf("Eolica: %d\n", numero2);
+
+    fscanf(archivo2, "%d", &numero3);
+    printf("Solar Fotovoltaiva: %d\n", numero3);
+
+    fscanf(archivo2, "%d", &numero4);
+    printf("Solar Termica: %d\n", numero4);
+    
+    fscanf(archivo2, "%d", &numero5);
+    printf("Hidroelectrica: %d\n", numero5);
+    
+    fscanf(archivo2, "%d", &numero6);
+    printf("Otras Renovables: %d\n", numero6);
+
+    fscanf(archivo2, "%d", &numero7);
+    printf("Resiudos Renovables: %d\n\n", numero7);
+
+    fscanf(archivo2, "%d", &numero8);
+    printf("Generacion Renovable: %d\n\n\n", numero8);
+
+    fscanf(archivo2, "%d", &numero9);
+    printf("Turbinacion Bombeo: %d\n", numero9);
+
+    fscanf(archivo2, "%d", &numero10);
+    printf("Nuclear: %d\n", numero10);
+
+    fscanf(archivo2, "%d", &numero11);
+    printf("Ciclo Bombeado: %d\n", numero11);
+
+    fscanf(archivo2, "%d", &numero12);
+    printf("Carbon: %d\n", numero12);
+
+    fscanf(archivo2, "%d", &numero13);
+    printf("Motores Diesel: %d\n", numero13);
+
+    fscanf(archivo2, "%d", &numero14);
+    printf("Turbina de Gas: %d\n", numero14);
+
+    fscanf(archivo2, "%d", &numero15);
+    printf("Turbina de Vapor: %d\n", numero15);
+
+    fscanf(archivo2, "%d", &numero16);
+    printf("Fuel Gas: %d\n", numero16);
+
+    fscanf(archivo2, "%d", &numero17);
+    printf("Cogeneracion: %d\n", numero17);
+
+    fscanf(archivo2, "%d", &numero18);
+    printf("Residuos no renovables: %d\n\n", numero18);
+
+    fscanf(archivo2, "%d", &numero19);
+    printf("Generacion no renovable: %d\n\n\n", numero19);
+
+    printf("\n");
+
+    fclose(archivo1);
+    fclose(archivo2);
 }
 
 void f_comparacion_menu (char *eleccion_comparacion) /*Funcion Menu de Comparaciones*/ /*Esta dentro de una funcion para evitar que se produzca un bug de duplicado*/
